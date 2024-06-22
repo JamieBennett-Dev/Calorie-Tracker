99e34505b8d4877651bdf0ac756f0d5b86b06043
Jamie Bennett <jbennett.130602@gmail.com>
2024-06-22 17:14:30 +0000

    complete imports for foodtracker/views.py

--- foodtracker/views.py
+++ foodtracker/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django import forms
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from .models import User, Food, FoodCategory, FoodLog, Image, Weight
+from .forms import FoodForm, ImageForm
 
 # Create your views here.
 
