7b0cccde8a1d3d70817a834f1fc2805d89cae07a
Jamie Bennett <jbennett.130602@gmail.com>
2024-07-01 16:31:07 +0000

    more comments on py

--- foodtracker/models.py
+++ foodtracker/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class User(AbstractUser):
     def __str__(self):
-        return f'{self.username}'
+        return f'{self.username}' # Returns the user their username
 
 class FoodCategory(models.Model):
     category_name = models.CharField(max_length=50)
