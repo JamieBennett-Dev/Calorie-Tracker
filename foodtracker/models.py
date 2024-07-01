cf9d3b8fc3cfbc55f1de631a7af162791579d092
Jamie Bennett <jbennett.130602@gmail.com>
2024-07-01 14:21:50 +0000

    update models.py

--- foodtracker/models.py
+++ foodtracker/models.py
@@ -1,24 +1,7 @@
-from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
-
 class User(AbstractUser):
-    groups = models.ManyToManyField(
-        Group,
-        related_name='foodtracker_user_groups',  # Unique related_name
-        blank=True,
-        help_text=('The groups this user belongs to. A user will get all permissions granted to each of their groups.'),
-        related_query_name='user',
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        related_name='foodtracker_user_permissions',  # Unique related_name
-        blank=True,
-        help_text=('Specific permissions for this user.'),
-        related_query_name='user',
-    )
-
     def __str__(self):
         return f'{self.username}'
 
