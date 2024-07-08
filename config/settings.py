cf37f955ffadcda2b5bd91648e17af5d943e9e3a
Jamie Bennett <jbennett.130602@gmail.com>
2024-07-08 13:26:22 +0000

    install whitenoise

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-jamiebennet-calorietrac-iqjv5ukp5f4.ws.codeinstitute-ide.net',
@@ -138,13 +138,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 
-# Location where Django collects all static files 
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Location where we will store our static files 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
