97677a396033361292e89b065b2849b4b4ff6476
Jamie Bennett <jbennett.130602@gmail.com>
2024-07-15 09:43:39 +0000

    migrated database to sqlite

--- config/settings.py
+++ config/settings.py
@@ -85,16 +85,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+   }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 
 CSRF_TRUSTED_ORIGINS = [
     "https://8000-jamiebennet-calorietrac-iqjv5ukp5f4.ws.codeinstitute-ide.net",
